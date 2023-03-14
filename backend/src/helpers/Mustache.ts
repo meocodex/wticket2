8a02b2b23375264a46ca3e188da43b6f42c443c0
MTech® Sistemas <114046464+MTechSistemas@users.noreply.github.com>
2023-03-14 01:55:33 -0300

    Update Mustache.ts

--- backend/src/helpers/Mustache.ts
+++ backend/src/helpers/Mustache.ts
@@ -1,6 +1,6 @@
 import Mustache from "mustache";
 import Contact from "../models/Contact";
-import User from "../models/User";
+import UserQueue from "../models/UserQueue";
 
 export const greeting = (): string => {
   const greetings = ["Boa madrugada", "Bom dia", "Boa tarde", "Boa noite"];
@@ -9,7 +9,7 @@ export const greeting = (): string => {
   return greetings[(h / 6) >> 0];
 };
 
-export default (body: string, contact: Contact, user: User): string => {
+export default (body: string, contact: Contact, userqueue: UserQueue): string => {
   let ms = "";
 
   const Hr = new Date();
@@ -40,7 +40,7 @@ export default (body: string, contact: Contact, user: User): string => {
 
   const view = {
     name: contact ? contact.name : "",
-	nameuser: user ? user.name : "",
+	nameuser: userqueue ? userqueue.userId : "",
     gretting: greeting(),
     ms,
     protocol,
